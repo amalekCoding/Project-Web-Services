c3e7edabdf038087de8a843c450822e5980c50c3
Samy WADAN <SaladeTomatOignon@hotmail.com>
2020-11-23 21:30:51 +0100

    Unique instance d'un employé

--- EiffelCorp/src/main/java/fr/uge/eiffelCorp/Employee.java
+++ EiffelCorp/src/main/java/fr/uge/eiffelCorp/Employee.java
@@ -2,13 +2,24 @@ package fr.uge.eiffelCorp;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import fr.uge.ifsCars.Tenant;
 
 public class Employee extends UnicastRemoteObject implements Tenant {
 	private final long id;
+	private static final Map<Long, Employee> instances = new ConcurrentHashMap<Long, Employee>();
 	
-	public Employee(long id) throws RemoteException {
+	public static Employee getEmployee(long id) throws RemoteException {
+		if (!instances.containsKey(id)) {
+			instances.put(id, new Employee(id));
+		}
+		
+		return instances.get(id);
+	}
+	
+	private Employee(long id) throws RemoteException {
 		super();
 		this.id = id;
 	}
